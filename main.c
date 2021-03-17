4f74411f23087ddd9f059270b76696f0c6ced702
JuliaChae <julia.chae2000@gmail.com>
2021-03-17 13:59:25 -0400

    Added splashscreen

--- main.c
+++ main.c
@@ -1,39 +1,34 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include "arrow_sprite.c"
-#include "menudata.c"
-#include "menu.c"
-#include "arrow.c"
+#include "sprites/smile.c"
+#include "menu/menudata.c"
+#include "menu/menu.c"
+#include "sprites/arrow.c"
+#include "sprites/arrow_sprite.c"
+#include "backgrounds/letters.c"
+#include "backgrounds/endscreen.c"
+#include "backgrounds/splashscreen_data.c"
+#include "backgrounds/splashscreen_map.c"
+#include "backgrounds/blankscreen.c"
+
 
 struct Arrow arrow; 
+struct Smile smile; 
 UBYTE keydown; 
-void performantdelay(UINT8 numloops){
-    UINT8 ii;
-    for(ii=0; ii<numloops; ii++){
-        wait_vbl_done();
-    }
-}
+UINT8 menuOption; 
+UINT8 gameOn = 1;
 
-void main(){
-    UINT8 currentspriteindex = 0;
 
-    set_sprite_data(0, 1, ArrowSprite);
-    set_sprite_tile(0, 0);
-    
-    arrow.x = 120;
-    arrow.y = 32;
-    arrow.col = 0;
-    arrow.row = 0;
-    move_sprite(0, arrow.x, arrow.y);
-
+void load_menu(){
     set_bkg_data(0,19,MenuTiles);
     set_bkg_tiles(12,0,8,10, Menu);
-    ///scroll_bkg(-4,0);
+    set_sprite_data(2, 1, ArrowSprite);
+    set_sprite_tile(1, 2);
+    
+    arrow.row = 0;
 
-    SHOW_BKG;
-    SHOW_SPRITES;
-    DISPLAY_ON; 
-
+    move_sprite(1, 120, 32);
+ 
     while(1){
         if(keydown){
             waitpadup();
@@ -41,20 +36,83 @@ void main(){
         }
         switch(joypad()){
             case J_UP:
-                arrow.y -= 8;
-                arrow.row--;
-                scroll_sprite(0,0,-8); 
-                keydown = 1;
+                if (arrow.row > 0)
+                {   arrow.row--;
+                    scroll_sprite(1,0,-8); 
+                    keydown = 1;
+                }
                 break;
             case J_DOWN:
-                arrow.y += 8;
-                arrow.row++;
-                scroll_sprite(0,0,+8); 
-                keydown = 1;
+                if (arrow.row < 2){
+                    arrow.row++;
+                    scroll_sprite(1,0,+8); 
+                    keydown = 1;
+                }
                 break;
+            case J_START:
+                menuOption = arrow.row;
+                return; 
         }
-        performantdelay(2);
     }
+}
 
+void resumeGame(){
+    move_sprite(0, smile.x, smile.y); 
 
+    set_bkg_data(0,19,MenuTiles);
+    set_bkg_tiles(0, 0, 40, 18, blankMap);
+}
+
+void main(){
+    UINT8 currentspriteindex = 0;
+    set_bkg_data(0,114,splashscreen_data);
+    set_bkg_tiles(0,0,20,20,splashscreen_map);
+    SHOW_BKG;
+    DISPLAY_ON;
+    waitpad(J_START);
+
+    set_bkg_data(0,19,MenuTiles);
+    set_bkg_tiles(0, 0, 20, 18, blankMap);
+
+    set_sprite_data(0, 2, Smiler);
+    set_sprite_tile(0, 0);
+    
+    smile.x = 88;
+    smile.y = 78;
+    move_sprite(0, 88, 78);
+    SHOW_SPRITES;
+    SHOW_BKG;
+    DISPLAY_ON; 
+
+    while(gameOn == 1){
+        switch(joypad()){
+            case J_LEFT: 
+                scroll_sprite(0,-1,0);
+                smile.x -=1;
+                break;
+            case J_RIGHT: 
+                scroll_sprite(0,1,0);
+                smile.x += 1; 
+                break;  
+            case J_UP: 
+                scroll_sprite(0,0,-1);
+                smile.y -= 1; 
+                break; 
+            case J_DOWN: 
+                scroll_sprite(0,0,1);
+                smile.y += 1;
+                break;  
+            case J_A:
+                load_menu();
+                if (menuOption == 2){
+                    gameOn = 2;
+                } else{
+                    resumeGame();
+                }
+                break;                                                 
+        }
+        delay(100);
+    }
+    set_bkg_data(0, 27, letterTiles);
+    set_bkg_tiles(0, 0, 20, 18, endMap);   
 }
--- make copy.bat
+++
@@ -1,2 +0,0 @@
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o game.o game.c
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o game.gb game.o
