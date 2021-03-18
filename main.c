6d05001059e5e84d2a9fd272a9e1d0148d04b5af
joelckahn <32375920+joelckahn@users.noreply.github.com>
2021-03-18 13:31:58 -0400

    Combat Level- hard difficulty

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <gb/gb.h>
+#include <stdio.h>
+#include "bkgcirclesmap.c"
+#include "bkgcircletiles.c"
+#include "GameCharacter.c"
+#include "GameSprites.c"
+#include "HeatSprites.c"
+#include "levelOne.c"
+
+
+void main(){
+    levelOne();
+}
