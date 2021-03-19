9c785ab846b10ade877455ccc72c8e21e1f37b1c
JuliaChae <julia.chae2000@gmail.com>
2021-03-19 00:21:17 -0400

    Adding background and menu

---
+++ Test_Background/README.md
@@ -0,0 +1,2 @@
+# MIE438-GameBoy-Project
+Main project for University of Toronto's course MIE438: Microcontrollers and Embedder Processors
---
+++ Test_Background/make.bat
@@ -0,0 +1,9 @@
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o BackgroundData.o BackgroundData.c 
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o BackgroundMap.o BackgroundMap.c 
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o main.o main.c 
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -Wm-yc -o gbcbackground_v2.gb BackgroundMap.o BackgroundData.o main.o
+
+DEL *.lst
+DEL *.o
+DEL *.map
+DEL *.sym
