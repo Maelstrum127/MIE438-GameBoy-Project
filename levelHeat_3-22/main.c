987fb66badbf21984cd5d5e64c3e99c82ae976c8
joelckahn <32375920+joelckahn@users.noreply.github.com>
2021-03-22 19:05:48 -0400

    Add files via upload

---
+++ levelHeat_3-22/main.c
@@ -0,0 +1,18 @@
+#include <gb/gb.h>
+#include <gb/cgb.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "GameCharacter.c"
+#include "EnemyCharacter.c"
+#include "GameSprites.h"
+#include "HeatSprites.c"
+#include "spriteBitAnimation.c"
+#include "combatLevels.c"
+#include "levelHeat.c"
+
+
+
+void main(){
+
+    levelHeat();
+}
