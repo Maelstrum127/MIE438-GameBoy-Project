803f4135610cba05e9e5931936b7bd14b6f6f7ff
joelckahn <32375920+joelckahn@users.noreply.github.com>
2021-03-21 21:37:26 -0400

    Delete levelHeat_3-21 directory

--- levelHeat_3-21/make.bat
+++
@@ -1,8 +0,0 @@
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o GameSprites.o GameSprites.c 
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o main.o main.c 
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -Wm-yc -o gbcSprite.gb GameSprites.o main.o
-
-DEL *.lst
-DEL *.o
-DEL *.map
-DEL *.sym
